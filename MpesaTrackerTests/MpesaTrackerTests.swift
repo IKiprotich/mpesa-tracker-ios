14461043d4a6065aea1613311e02cc241ead58a5
Ian Kiprotich <kiprotichian254@gmail.com>
2026-05-19 10:53:12 +0300

    feat: add ParsedRow value type with signed amount and uniqueKey

---
+++ MpesaTrackerTests/MpesaTrackerTests.swift
@@ -0,0 +1,18 @@
+//
+//  MpesaTrackerTests.swift
+//  MpesaTrackerTests
+//
+//  Created by Ian Kiprotich on 19/05/2026.
+//
+
+import Testing
+
+struct MpesaTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
---
+++ MpesaTrackerTests/TransactionParserTests.swift
@@ -0,0 +1,11 @@
+@testable import PesaTracker
+import XCTest
+
+class MPesaTests: XCTestCase {
+
+    func testExample() {
+        // Example test case
+        XCTAssertEqual(2 + 2, 4)
+    }
+
+}
